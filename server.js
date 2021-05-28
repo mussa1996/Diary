8f31129b1d048af1ac0ad25414f811ae93fb580e
mussa1996 <niyodusengamussa01@gmail.com>
2021-05-28 11:45:04 +0200

    add welcome message

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ connectDb();
 app.use(express.json({extended:false}));
 app.use('/api',routes);
 app.use(bodyParser.json());
+app.get("/",function(req,res){
+    res.send("WELCOME TO MY DIARY")
+})
 const Port =process.env.port || 3000;
 app.listen(Port,()=>console.log("Server stated"));
