3433785a247fde3d66855344e691cba0ca9ddeb0
mussa1996 <niyodusengamussa01@gmail.com>
2021-05-28 11:56:32 +0200

    added dotenv to prevent our data

--- DB/dbconnection.js
+++ DB/dbconnection.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
-const URL="mongodb+srv://mussa:mussa@cluster0.pycek.mongodb.net/diary?retryWrites=true&w=majority";
+require('dotenv').config()
+const connectionURL =process.env.dbConnection
 const connectDb=async()=>{
    try { await mongoose.connect(URI,{
     useFindAndModify:true,
