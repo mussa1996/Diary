4dcfb5b1ed35cc74870ee2862ff7c952ee2d5e8b
mussa1996 <niyodusengamussa01@gmail.com>
2021-05-29 14:09:37 +0200

    add cors in server

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const routes =require('./route/index');
 const bodyParser=require('body-parser');
 const app=express();
 connectDb();
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json({extended:false}));
 app.use('/api',routes);
 app.use(bodyParser.json());
